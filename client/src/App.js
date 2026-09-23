e0c8fd0d5113d9e546dc3de1ed3e83fc209a97e1
agosGentille <agoosgentille07@gmail.com>
2026-09-23 11:23:25 -0300

    fix error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 
